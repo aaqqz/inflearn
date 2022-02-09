5a2f9819c427854ad1067223cc6174319939a6d2
aaqqz <sswwx1@naver.com>
2022-02-10 01:08:31 +0900

    조회 빈이 2개 이상 - 문제

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -7,14 +7,28 @@ import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // 롬복 기능
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    /* 생성자 주입 */ // 생성자 주입 제일 많이 사용
+    // @RequiredArgsConstructor에서 자동으로 생성자 주입 코드 만들어줌 - 확인 (ctrl + f12)(메소드 확인 단축키?)
+    // 실제 class 파일에 생성되어있다
+//    @Autowired // 생성자가 하나 일때 @Autowired 생략 가능
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+////        System.out.println("memberRepository = " + memberRepository);
+////        System.out.println("discountPolicy = " + discountPolicy);
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
+    /* 생성자 주입 */
 
     /* 일반 메서드 주입 */
 //    @Autowired
@@ -41,16 +55,6 @@ public class OrderServiceImpl implements OrderService{
 //        this.discountPolicy = discountPolicy;
 //    }
     /* 수정자 주입(setter 주입) */
-
-    /* 생성자 주입 */
-    @Autowired // 생성자가 하나 일때 @Autowired 생략 가능
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        System.out.println("memberRepository = " + memberRepository);
-//        System.out.println("discountPolicy = " + discountPolicy);
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-    /* 생성자 주입 */
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
