7ceebbc796120b867121db859aec42d3e57df93e
aaqqz <sswwx1@naver.com>
2022-06-30 02:07:39 +0900

    HTTP 요청 - 기본, 헤더 조회

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,34 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+
+//    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest(){
+        String name = "Spring";
+        String test = "Test";
+
+        System.out.println("name = " + name);
+
+        // log level 순서 (properties에서 설정 가능)
+        log.trace(" trace log={}", name);
+        log.debug(" debug log={}", name);
+        log.info(" info log={}", name);
+        log.warn(" warn log={}", name);
+        log.error(" error log={}", name);
+
+//        log.info(" info log={}", name);
+//        log.info(" info log={}, {}", name, test);
+
+        return "ok";
+    }
+}
