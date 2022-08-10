b3fec5bfccf19fd3b8d67a1c1b2028c35fba3fe8
HK-Kim <sswwx1@naver.com>
2022-08-11 01:35:03 +0900

    로그인 처리하기 - 서블릿 HTTP 세션1

--- src/main/java/hello/login/web/session/SessionManager.java
+++ src/main/java/hello/login/web/session/SessionManager.java
@@ -21,19 +21,13 @@ public class SessionManager {
 
     /**
      * 세션 생성
-     * sessionId 생성 (임의의 추정 불가능한 랜덤 값)
-     * 세션 저장소에 sessionId와 보관할 값 저장
-     * sessionId로 응답 쿠키를 생성해서 클라이언트에 전달
      */
     public void createSession(Object value, HttpServletResponse response) {
-
         //세션 id를 생성하고, 값을 세션에 저장
         String sessionId = UUID.randomUUID().toString();
         sessionStore.put(sessionId, value);
-
         //쿠키 생성
         Cookie mySessionCookie = new Cookie(SESSION_COOKIE_NAME, sessionId);
-
         response.addCookie(mySessionCookie);
     }
 
@@ -45,11 +39,20 @@ public class SessionManager {
         if (sessionCookie == null) {
             return null;
         }
-
-        return sessionStore.get(sessionCookie.getName());
+        return sessionStore.get(sessionCookie.getValue());
     }
 
-    public Cookie findCookie(HttpServletRequest request, String cookieName) {
+    /**
+     * 세션 만료
+     */
+    public void expire(HttpServletRequest request) {
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        if (sessionCookie != null) {
+            sessionStore.remove(sessionCookie.getValue());
+        }
+    }
+
+    private Cookie findCookie(HttpServletRequest request, String cookieName) {
         if (request.getCookies() == null) {
             return null;
         }
