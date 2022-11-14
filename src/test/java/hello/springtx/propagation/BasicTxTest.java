78727105f91e26be7b02dddf797c82bb89edfe14
HK-Kim <sswwx1@naver.com>
2022-11-14 22:38:24 +0900

    스프링 트랜잭션 전파6 - 내부 롤백

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -80,7 +80,7 @@ public class BasicTxTest {
         TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
         log.info("outer.isNewTransaction={}", outer.isNewTransaction());
 
-        log.info("내부 트랜잭션 시작");
+        log.info("내부 트랜잭션 시작"); // 내부는 외부에 참여한다
         TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
         log.info("inner.isNewTransaction={}", inner.isNewTransaction());
         log.info("내부 트랜잭션 커밋");
@@ -89,4 +89,20 @@ public class BasicTxTest {
         log.info("외부 트랜잭션 커밋");
         txManager.commit(outer);
     }
+
+    @Test
+    void outer_rollback() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("내부 트랜잭션 시작"); // 내부는 외부에 참여한다
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);
+
+        log.info("외부 트랜잭션 롤백");
+        txManager.rollback(outer);
+    }
+
+
 }
