6889f3093a6680a209fe75ba268780bc51035308
HK-Kim <sswwx1@naver.com>
2022-11-20 20:40:22 +0900

    트랜잭션 전파 활용2 - 커밋, 롤백

--- src/main/java/hello/springtx/propagation/LogRepository.java
+++ src/main/java/hello/springtx/propagation/LogRepository.java
@@ -2,11 +2,14 @@ package hello.springtx.propagation;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.util.Optional;
 
 @Slf4j
+@Repository
 @RequiredArgsConstructor
 public class LogRepository {
 
@@ -21,5 +24,11 @@ public class LogRepository {
             log.info("log 저장시 예외 발생");
             throw new RuntimeException("예외 발상");
         }
+    }
+
+    public Optional<Log> find(String message) {
+        return em.createQuery("select l from Log l where l.message = :message", Log.class)
+                .setParameter("message", message)
+                .getResultList().stream().findAny();
     }
 }
